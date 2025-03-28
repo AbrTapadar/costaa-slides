d12786cc56d2a205d400e0b06fc420c2d02cc7ab
Pranjal Gupta <pranjalgupta58.97@gmail.com>
2025-03-28 11:18:16 +0530

    second draft

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -9,7 +9,6 @@ gsap.registerPlugin(useGSAP);
 
 const Page = (props) => {
   const [indx, setindx] = useState("0");
-  const [show, setShow] = useState(false);
   const container = useRef();
   const { contextSafe } = useGSAP({
     scope: container,
@@ -23,75 +22,32 @@ const Page = (props) => {
   );
   const tl = timelineRef.current;
 
-  useEffect(() => {
-    const animation = contextSafe(() => {
-      tl.to(container.current, {
-        rotateX: 10,
-        rotateY: 10,
-        x: 15,
-        // transformOrigin: "0% 100%",
-      })
-        .to(container.current, {
-          rotateX: 0,
-          rotateY: 0,
-          x: 0,
-          // transformOrigin: "0% 100%",
-        })
-        .to(container.current, {
-          rotateX: -7,
-          rotateY: -7,
-          x: -15,
-
-          // transformOrigin: "100% 0%",
-        })
-        .to(container.current, {
-          rotateX: 0,
-          rotateY: 0,
-          x: 0,
-          // transformOrigin: "100% 0%",
-        });
+  const animation = contextSafe(() => {
+    tl.to(container.current, {
+      rotateX: 10,
+      rotateY: 10,
+      x: 15,
     });
-    animation();
-  }, []);
+  });
 
   useEffect(() => {
-    const transition = () => {
-      const timeline2 = gsap.timeline({
-        defaults: { duration: 2, ease: "sine.inOut" },
-        onComplete: () => {
-          tl.play();
-          setShow(true);
-        },
-      });
-      timeline2.to(container.current, {
-        rotateY: gsap.getProperty(container.current, "rotateY") + 360,
-        rotateX: gsap.getProperty(container.current, "rotateX"),
-        x: gsap.getProperty(container.current, "x"),
-        transformOrigin: "50% 50%",
-      });
-    };
+    // animation();
     if (props.index != indx) {
-      console.log(gsap.getProperty(container.current, "rotateY"));
-      setShow(false);
-      tl.pause();
-      transition();
       setindx(props.index);
     }
   }, [props.index]);
 
   return (
-    <div ref={container} className={styles.container}>
-      <img src={logo} alt="logo" className={styles.logo} />
-      <div className={styles.content}>
-        {show && (
-          <>
-            <img src={props.info.img} alt="CoStAA" className={styles.pic} />
-            <div className={styles.textBox}>
-              <h1>{props.info.name}</h1>
-              <h3>{props.info.department}</h3>
-            </div>
-          </>
-        )}
+    <div className={styles.container}>
+      <div className={styles.content} ref={container}>
+        <img src={logo} alt="logo" className={styles.logo} />
+        <div className={styles.content2}>
+          <img src={props.info.img} alt="CoStAA" className={styles.pic} />
+          <div className={styles.textBox}>
+            <h1>{props.info.name}</h1>
+            <h3>{props.info.department}</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
